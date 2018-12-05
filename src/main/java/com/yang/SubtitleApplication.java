b7a3d71b7e6762697c9ac9b3c49227c33990687d
KuanYangGH <ykxjtuedu@163.com>
2018-12-05 19:57:15 +0800

    保存必要文件

---
+++ src/main/java/com/yang/SubtitleApplication.java
@@ -0,0 +1,12 @@
+package com.yang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubtitleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubtitleApplication.class, args);
+    }
+}
